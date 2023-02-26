b974e0e8f4f5f193a596fafa22cc641d1f4b09c6
DrPeppr <md.purcaru@gmail.com>
2023-02-26 20:25:57 +0000

    solved

---
+++ script.js
@@ -0,0 +1,168 @@
+var characterLength = 10;
+var choiceArr = [];
+
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+  // console.log("Hey! You clicked the button!");
+
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+  }
+  console.log(password)
+  return password;
+
+//4. Display password
+  return "Generated password will go here!";
+}
+
+function getPrompts() {
+  choiceArr = [];
+
+  characterLength = parseInt(prompt("How many characters do you want your password to be? (10 - 64 characters)")) // NaN
+
+  if(isNaN(characterLength)|| characterLength < 10 || characterLength > 64) {
+    alert("Character length has to be between 10 - 64 digits. Please try again.");
+    return false;
+  }
+
+  if (confirm("Would you like lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCasedCharacters);
+  }
+
+  if (confirm("Would you like uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(upperCasedCharacters);
+  }
+
+  if (confirm("Would you like special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharacters);
+  }
+
+  if (confirm("Would you like numbers in your password?")) {
+    choiceArr = choiceArr.concat(numericCharacters);
+  }
+  console.log(choiceArr)
+return choiceArr;
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+    var correctPrompts = getPrompts(); // returns true or false
+    var passwordText = document.querySelector('#password');
+
+    if(correctPrompts) {
+      var newPassword = generatePassword();
+      
+
+  passwordText.value = newPassword;
+    } else {
+      passwordText.value = "";
+    }
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
